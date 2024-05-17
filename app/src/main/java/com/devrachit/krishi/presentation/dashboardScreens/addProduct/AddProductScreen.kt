1bc7030e1126b2c7c41d0b804c8f59727155b51d
rockyhappy <115190222+rockyhappy@users.noreply.github.com>
2024-05-18 04:04:54 +0530

    Screen Files Created

---
+++ app/src/main/java/com/devrachit/krishi/presentation/dashboardScreens/addProduct/AddProductScreen.kt
@@ -0,0 +1,10 @@
+package com.devrachit.krishi.presentation.dashboardScreens.addProduct
+
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+
+@Composable
+fun AddProductScreen(navController: NavController) {
+    val viewModel: AddProductViewModel = hiltViewModel()
+}
---
+++ app/src/main/java/com/devrachit/krishi/presentation/dashboardScreens/addProduct/AddProductViewModel.kt
@@ -0,0 +1,22 @@
+package com.devrachit.krishi.presentation.dashboardScreens.addProduct
+
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.lifecycle.ViewModel
+import com.devrachit.krishi.domain.models.SharedViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class AddProductViewModel @Inject constructor(
+    val sharedViewModel: SharedViewModel,
+    val dataStore: DataStore<Preferences>,
+    val auth: FirebaseAuth,
+    val storage: FirebaseStorage,
+    val db: FirebaseFirestore,
+) : ViewModel(){
+
+}
